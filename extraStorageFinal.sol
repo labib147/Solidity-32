028fd956f35b54be566f78561d1b125506f1f99a
Nefaur Rahman Labib <52388164+labib147@users.noreply.github.com>
2022-08-31 16:01:57 +0600

    Create extraStorageFinal.sol

---
+++ extraStorageFinal.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./simpleStorage.sol";
+
+
+// contract extraStorage inherits all the functions of simpleStorage, acts a child to SS
+contract extraStorage is simpleStorage {
+    // virtual & override
+    function store (uint256 _fNum) public override {
+        fNum = _fNum +5 ;
+    }
+
+
+}
